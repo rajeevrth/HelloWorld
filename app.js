ff513f9c6b6706a986c71279170e526d71ed3192
rajeevrth <rajeevkumarsharma@outlook.com>
2017-03-17 13:50:29 +0530

    javascript tut

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ for (let i = 0; i < dataTypes.length; i++) {
 
 // Array<string>.splice(start: number, deleteCount?: number)
 // Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
-dataTypes.splice(1,3);
+dataTypes.splice(1, 3);
 console.log(dataTypes);
 //Output : 
 // 0
@@ -33,7 +33,7 @@ dataTypes.push("String", "Boolean", "Objects");
 
 // Array<string>.slice(start?: number, end?: number)
 // Returns a section of an array.
-var newArray=dataTypes.slice(1,3);
+var newArray = dataTypes.slice(1, 3);
 console.log(newArray);
 // Output:
 // 0
@@ -46,4 +46,22 @@ console.log(newArray);
 // :
 // 2
 
+// Spread syntax.
+function Avg(...args) {
+    let sum = 0;
+    for (let val of args) {
+        sum = + val;
+    }
+    sum = sum / args.length;
+    console.log(sum);
+}
+// In the above code the variable args holds all the values that were passed into the function. 
+Avg(2, 3, 5);
 
+// anonymous functions.
+var average = function (a, b, c) {
+    let sum = 0;
+    sum = a + b + c / 3;
+    return sum;
+}
+console.log(average(2, 3, 4))
