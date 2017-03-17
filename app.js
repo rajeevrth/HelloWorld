1ef898fc8c7ce0291c838f785799b797db868f9a
rajeevrth <rajeevkumarsharma@outlook.com>
2017-03-17 11:29:05 +0530

    javascript dom addition

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 // If you declare a variable, without using "var", the variable always becomes GLOBAL.
 x=0;
+var dataTypes=["Number","String", "Boolean", "Objects", "null", "undefined"];
+var para = document.createElement("p");
+ var br = document.createElement("br");
+for(let i=0; i<dataTypes.length; i++)
+{
+var node = document.createTextNode(dataTypes[i]);
+para.appendChild(node);
+para.appendChild(document.createElement("br"));
+var element = document.getElementById("datatype");
+element.appendChild(para);
+}
 
-alert("hello world");
+
+
